806befe641fadc95541535c8a5bf5a0d47501b48
Todd Lomas <todd.lomas@hotmail.co.uk>
2022-11-03 10:25:45 +0000

    End of Express work

--- NodeJs/ThirdApp/index.js
+++ NodeJs/ThirdApp/index.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
+uuid();
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const comments = [
-  {
-    username: "Todd",
-    comment: "lol thats me",
-  },
-  {
-    username: "Andy",
-    comment: "What on earth",
-  },
-  {
-    username: "Helen",
-    comment: "I am mother",
-  },
-  {
-    username: "Jack",
-    comment: "I like dogs",
-  },
+let comments = [
+  { id: uuid(), username: "Todd", comment: "lol thats me" },
+  { id: uuid(), username: "Andy", comment: "What on earth" },
+  { id: uuid(), username: "Helen", comment: "I am mother" },
+  { id: uuid(), username: "Jack", comment: "I like dogs" },
 ];
 
 app.get("/comments", (req, res) => {
@@ -34,19 +26,36 @@ app.get("/comments/new", (req, res) => {
   res.render("comments/new", { comments });
 });
 
-app.get("/tacos", (req, res) => {
-  res.send("GET /tacos response");
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment, id });
 });
 
-app.post("/tacos", (req, res) => {
-  const { meat, qty } = req.body;
-  res.send(`Here are your ${qty} ${meat} tacos`);
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment, id });
 });
 
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
-  res.send(`It Worked!`);
+  comments.push({ username, comment, id: uuid() });
+  res.redirect("/comments");
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect("/comments");
 });
 
 app.listen(3000, () => {
