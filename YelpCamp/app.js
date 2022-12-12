014a78ed83eba01cff8452f0f4b290c29b993ea5
Todd Lomas <todd.lomas@hotmail.co.uk>
2022-12-12 18:14:04 +0000

    correct url

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -17,8 +17,8 @@ const userRoutes = require("./routes/users");
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const MongoStore = require("connect-mongo");
-//const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL;
+// const dbUrl = "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(dbUrl);
 
